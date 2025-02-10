0d853ac30f7bbde8a52cb88c62f8630175de9e74
petargeorgiev18 <156835211+petargeorgiev18@users.noreply.github.com>
2025-02-10 15:30:51 +0200

    Update Player.cs

--- Task_4.01/Task_4.01/Player.cs
+++ Task_4.01/Task_4.01/Player.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,22 +8,22 @@ namespace Task_4._01
 {
     public class Player:IPlayer
     {
-		private string name;
-		public string Name
-		{
-			get { return name; }
-			set { name = value; }
-		}
-		private string position;
-		public string Position
-		{
-			get { return position; }
-			set { position = value; }
-		}
+	private string name;
+	public string Name
+	{
+	        get { return name; }
+		set { name = value; }
+	}
+	private string position;
+	public string Position
+	{
+		get { return position; }
+		set { position = value; }
+	}
         public Player(string name, string position)
         {
             Name = name;
-			Position = position;
+	    Position = position;
         }
     }
 }
