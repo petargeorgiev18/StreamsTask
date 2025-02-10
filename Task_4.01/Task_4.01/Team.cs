c77b676389d5e0a93c34de787f8e62256ca3416d
petargeorgiev18 <156835211+petargeorgiev18@users.noreply.github.com>
2025-02-10 15:29:53 +0200

    Update Team.cs

--- Task_4.01/Task_4.01/Team.cs
+++ Task_4.01/Task_4.01/Team.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,37 +8,43 @@ namespace Task_4._01
 {
     public class Team
     {
-		private string name;
-		public string Name
-		{
-			get { return name; }
-			set { name = value; }
-		}
+	private string name;
+	public string Name
+	{
+		get { return name; }
+		set { name = value; }
+	}
         public List<IPlayer> Players { get; set; }
         private ILog log;
+        private ILog exceLog;
         public Team(string name)
         {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             Name = name;
             Players = new List<IPlayer>();
-            log = new TextLog();
-            log.Log($"Team {name} created at {DateTime.Now}");
+            log = new TextLog(@$"{path}\file.txt");
+            exceLog = new ExcelLogger(@$"{path}\file.xlsx");
+            string logMessage = $"Team {name} created at {DateTime.Now}";
+            log.Log(logMessage);
+            exceLog.Log(logMessage);
         }
-        public void add_player(string playerName, string position)
+        public void add_player(IPlayer playerToAdd)
         {
-            IPlayer player = Players.FirstOrDefault(p => p.Name == playerName);
+            IPlayer player = Players.FirstOrDefault(p => p.Name == playerToAdd.Name && p.Position == playerToAdd.Position);
             if (player == null)
             {
-                player = new Player(playerName, position);
-                Players.Add(player);
-                string logMessage = $"Player {playerName} with position {position} added to team " +
+                Players.Add(playerToAdd);
+                string logMessage = $"Player {playerToAdd.Name} with position {playerToAdd.Position} added to team " +
                     $"{Name} at {DateTime.Now}";
                 log.Log(logMessage);
+                exceLog.Log(logMessage);
                 Console.WriteLine(logMessage);
             }
             else
             {
                 string logMessage = $"The player {player.Name} already exists in team {Name} at {DateTime.Now}";
                 log.Log(logMessage);
+                exceLog.Log(logMessage);
                 Console.WriteLine(logMessage);
             }
         }
@@ -50,23 +56,48 @@ namespace Task_4._01
                 Players.Remove(player);
                 string logMessage = $"Player {player.Name} removed from team {Name} at {DateTime.Now}";
                 log.Log(logMessage);
+                exceLog.Log(logMessage);
                 Console.WriteLine(logMessage);
             }
             else
             {
                 string logMessage = $"No player named {playerName} found in team {Name} at {DateTime.Now}";
                 log.Log(logMessage);
+                exceLog.Log(logMessage);
                 Console.WriteLine(logMessage);
             }
         }
-        public void SaveLogToFile(string filePath)
+        public void SaveLogToFile(string fileType)
         {
-            log.SaveLog(filePath);
+            switch (fileType)
+            {
+                case "txt":
+                    log.SaveLog();
+                    break;
+                case "excel":
+                    exceLog.SaveLog();
+                    break;
+                default:
+                    Console.WriteLine($"Not existent type to log - {fileType}");
+                    return;
+            }
+            
         }
-        public void PrintLogToConsole()
+        public void PrintLogToConsole(string fileType)
         {
-            Console.WriteLine($"Log history for team {Name}:");
-            Console.WriteLine(log.GetLog());
+            Console.WriteLine($"Log history for team {this.Name}:");
+            switch (fileType)
+            {
+                case "txt":
+                    Console.WriteLine(log.GetLog());
+                    break;
+                case "excel":
+                    Console.WriteLine(exceLog.GetLog());
+                    break;
+                default:
+                    Console.WriteLine($"Not existent type to log - {fileType}");
+                    return;
+            }
         }
     }
 }
